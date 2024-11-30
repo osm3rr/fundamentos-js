4577dd9b8553790fc87b4553d796f8770201085e
osm3rr <osm3rr@mail.com>
2024-11-30 16:04:16 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// Variables
+// let MyName;
+// MyName = "josefino";
+
+// let MyAge = 15;
+
+// let MyName = "josefino2",
+//     MyAge = 15,
+//     MyMessage = "hello :)";
+
+// // alert(MyName);
+// // alert(MyAge);
+// // alert(MyMessage);
+
+// // constantes
+// const MyBirthDay = "Apr-21-2014";
+
+// // alert(MyBirthDay);
+
+// // tipos de datos
+// let MyAge2 = 33;
+// let IsActive = true;
+// let IsBlue = false;
+
+// let MyHeight = null;
+// let MyHeight2 = undefined;
+
+// // ingreso de argumentos
+// let year = prompt('año actual:');
+// // alert(year);
+
+// // confirm
+// let isBoss = confirm("Eres el jefe?");
+// // alert(isBoss);
+
+// // operadores matematicos
+// let Num1 = 10;
+// let Num2 = 20;
+
+// // suma
+// // alert( Num1 + Num2 );
+
+// // // resta
+// // alert( Num1 - Num2 );
+
+// // // multiplicación
+// // alert( Num1 * Num2 );
+
+// // // división
+// // alert( Num1 / Num2 );
+
+// // incremento
+// let Num3 = 2;
+// Num3++;
+// // alert(Num3);
+
+// operadores de comparación
+// alert( 2 > 1 ); 
+// alert( 2 < 1 );
+// alert( 2 == 1 );
+// alert( 2 !=  1);
+
+// condicionales
